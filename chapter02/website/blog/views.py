5ac73a8fc3d9e4d96791bc09088605cc580e58ba
Fco. Javier Fdez. Collados <xabyer.b@gmail>
2025-03-28 11:44:16 +0100

    Manejados los errores de paginación al poner datos en la url.

--- chapter02/website/blog/views.py
+++ chapter02/website/blog/views.py
@@ -1,18 +1,27 @@
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import get_object_or_404, render
 
 from .models import Post
 
 PAGE = 3
 
+
 # Create your views here.
 def post_list(request):
     post_list = Post.published.all()
 
-    #Pagination with 3 post per page
+    # Pagination with 3 post per page
     paginator = Paginator(post_list, PAGE)
     page_number = request.GET.get('page', 1)
-    posts = paginator.page(page_number)
+
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        # If page_number is not an integer get the first page.
+        posts = paginator.page(1)
+    except EmptyPage:
+        # if page_number is out of range get last page of results
+        posts = paginator.page(paginator.num_pages)
 
     return render(
         request,
@@ -20,14 +29,15 @@ def post_list(request):
         {'posts': posts},
     )
 
+
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(
         Post,
-        status= Post.Status.PUBLISHED,
-        slug = post,
-        publish__year = year,
-        publish__month = month,
-        publish__day = day
+        status=Post.Status.PUBLISHED,
+        slug=post,
+        publish__year=year,
+        publish__month=month,
+        publish__day=day
     )
     # try:
     #     post = Post.published.get(id=id)
