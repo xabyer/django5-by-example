d53097f80f298102dec1ca3f85e47ec8dc9b1440
Fco. Javier Fdez. Collados <xabyer.b@gmail>
2025-03-31 10:31:03 +0200

    Building a search view finished

--- chapter03/website/blog/forms.py
+++ chapter03/website/blog/forms.py
@@ -16,3 +16,7 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['name', 'email', 'body']
+
+
+class SearchForm(forms.Form):
+    query = forms.CharField()
--- chapter03/website/blog/views.py
+++ chapter03/website/blog/views.py
@@ -1,3 +1,4 @@
+from django.contrib.postgres.search import SearchVector, TrigramSimilarity
 from django.core.mail import send_mail
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.db.models import Count
@@ -6,7 +7,7 @@ from django.views.decorators.http import require_POST
 from django.views.generic import ListView
 from taggit.models import Tag
 
-from .forms import EmailPostForm, CommentForm
+from .forms import EmailPostForm, CommentForm, SearchForm
 from .models import Post
 
 PAGE = 3
@@ -165,4 +166,31 @@ def post_comment(request, post_id):
             'form': form,
             'comment': comment
         },
+    )
+
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            results = (
+                Post.published.annotate(
+                    search=SearchVector('title', 'body'),
+                )
+                .filter(search=query)
+            )
+
+    return render(
+        request,
+        'blog/post/search.html',
+        {
+            'form': form,
+            'query': query,
+            'results': results
+        }
     )
