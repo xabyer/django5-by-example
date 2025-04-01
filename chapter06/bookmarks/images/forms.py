2d6285ac648e3ad5cf7a77bd23b051b939a66800
xabyer <xabyer.b@gmail>
2025-04-01 11:44:03 +0200

    image forms up

--- chapter06/bookmarks/images/forms.py
+++ chapter06/bookmarks/images/forms.py
@@ -10,3 +10,13 @@ class ImageCreateForm(forms.ModelForm):
         widgets = {
             'url': forms.HiddenInput,
         }
+
+    def clean_url(self):
+            url = self.cleaned_data['url']
+            valid_extensions = ['jpg', 'jpeg', 'png']
+            extension = url.rsplig('.', 1)[1].lower()
+            if extension not in valid_extensions:
+                raise forms.ValidationError(
+                    'The given URL does not match valid image extensions.'
+                )
+            return url
